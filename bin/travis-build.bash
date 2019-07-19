80c6210d4f4313902bd857eda21186a35ba2839a
Bart Saelen <bart.saelen@geosolutions.be>
2019-07-19 15:43:24 +0200

    update travis build

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -10,7 +10,7 @@ sudo apt-get install solr-jetty
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
-export latest_ckan_release_branch=`git branch --all | grep remotes/origin/release-v | sort -r | sed 's/remotes\/origin\///g' | head -n 1`
+export latest_ckan_release_branch=`git branch --all | grep remotes/origin/ | sort -r | sed 's/remotes\/origin\///g' | head -n 1`
 echo "CKAN branch: $latest_ckan_release_branch"
 git checkout $latest_ckan_release_branch
 python setup.py develop
