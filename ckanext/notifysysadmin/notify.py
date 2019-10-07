7b6f08b8d1c5f3423e1131c7816d1893185bf520
Bart Saelen <bart.saelen@geosolutions.be>
2019-10-07 09:51:29 +0200

    fix Notify werkt niet met speciale karakters.

--- ckanext/notifysysadmin/notify.py
+++ ckanext/notifysysadmin/notify.py
@@ -11,8 +11,8 @@ NOTIFICATION_SPACING = 45
 def notify_sysadmins(sysadmins, entity):
     site_url = config.get('ckan.site_url', "")
     organization_url = "{0}/organization/{1}"
-    subject = "New organization {0} created!".format(entity.title)
-    message = """
+    subject = u"New organization {0} created!".format(entity.title)
+    message = u"""
     A new organization {0} has been created on {1}.
     As a system administrator, you can delete this organization if it doesn't fulfill the required standards. 
     
@@ -32,15 +32,15 @@ def notify_sysadmins(sysadmins, entity):
 
 
 def print_notification(entity):
-    log.info("Organisation {0} created".format(entity.name))
+    log.info(u"Organisation {0} created".format(entity.name))
     log.info(_notification_line_spacer())
-    log.info("| id          | {0} |".format(_notification_format_value(entity.id)))
+    log.info(u"| id          | {0} |".format(_notification_format_value(entity.id)))
     log.info(_notification_line_spacer())
-    log.info("| name        | {0} |".format(_notification_format_value(entity.name)))
+    log.info(u"| name        | {0} |".format(_notification_format_value(entity.name)))
     log.info(_notification_line_spacer())
-    log.info("| title       | {0} |".format(_notification_format_value(entity.title)))
+    log.info(u"| title       | {0} |".format(_notification_format_value(entity.title)))
     log.info(_notification_line_spacer())
-    log.info("| created     | {0} |".format(_notification_format_value(entity.created.strftime("%d-%m-%Y, %H:%M:%S"))))
+    log.info(u"| created     | {0} |".format(_notification_format_value(entity.created.strftime("%d-%m-%Y, %H:%M:%S"))))
     log.info(_notification_line_spacer())
 
 
