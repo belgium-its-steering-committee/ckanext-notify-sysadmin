a7e29b703edc7f02bd69d47bdaf6fb86608c782f
Bart Saelen <bart.saelen@geosolutions.be>
2020-01-21 16:38:08 +0100

    check for None

--- ckanext/notifysysadmin/sysadmin.py
+++ ckanext/notifysysadmin/sysadmin.py
@@ -3,5 +3,9 @@ from ckan import model
 
 def get_ckan_sysadmins():
     sysadmins = set(model.Session.query(model.User).filter(model.User.sysadmin == True))
-    sysadmins = set([sysadmin for sysadmin in sysadmins if not sysadmin.email.find("@localhost")])
+    for sysadmin in sysadmins:
+        print(sysadmin)
+        print(sysadmin.email)
+    sysadmins = set([sysadmin for sysadmin in sysadmins if sysadmin.email
+                     is not None and not sysadmin.email.find("@localhost")])
     return sysadmins
