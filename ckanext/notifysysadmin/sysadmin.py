717c751145a0f3568206bed020e1d2ef5ef3379a
Bart Saelen <bart.saelen@geosolutions.be>
2020-01-21 16:30:10 +0100

    filter out 'localhost' users

--- ckanext/notifysysadmin/sysadmin.py
+++ ckanext/notifysysadmin/sysadmin.py
@@ -3,4 +3,5 @@ from ckan import model
 
 def get_ckan_sysadmins():
     sysadmins = set(model.Session.query(model.User).filter(model.User.sysadmin == True))
+    sysadmins = set([sysadmin for sysadmin in sysadmins if not sysadmin.email.find("@localhost")])
     return sysadmins
